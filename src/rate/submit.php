89a4d4736af8d1a6e40ed593f22ae4eca130766e
Tim Jacomb <timjacomb1@gmail.com>
2025-12-02 10:10:51 +0000

    Trim trailing /

--- src/rate/submit.php
+++ src/rate/submit.php
@@ -12,8 +12,9 @@
       $row = array('version' => $_GET['version'], 'voter' => $voter,
                    'rollback' => $_GET['rating'] == -1);
 
-      if (isset($_GET['issue']) && preg_match('#^https://github\.com/jenkinsci/[a-zA-Z0-9._-]+/issues/(\d+)$#', $_GET['issue'], $matches)) {
-        $row['issue'] = $_GET['issue'];
+      $issue = rtrim($_GET['issue'], '/');
+      if (isset($_GET['issue']) && preg_match('#^https://github\.com/jenkinsci/[a-zA-Z0-9._-]+/issues/(\d+)$#', $issue, $matches)) {
+        $row['issue'] = $issue;
       } else {
         $issue = (int)preg_replace('/^(JENKINS|HUDSON)-/i', '', $_GET['issue']);
         if ($issue > 0) $row['issue'] = $issue;
